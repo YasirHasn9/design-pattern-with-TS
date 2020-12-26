cf6946bfc46d55e807e2a03ea2b9ee0afc3fc66d
Yasir Hassan <hasanyasir01@gmail.com>
2020-12-26 11:12:59 -0800

    sort array using clasess

--- build/index.js
+++ build/index.js
@@ -1,2 +1,41 @@
 "use strict";
-console.log("hi there");
+function bubbleSort(arr) {
+    var _a;
+    // loop over the array 
+    // pick 2 numbers in array and compare them
+    // if the left > right: 
+    //    switch places
+    for (var i = 1; i < arr.length; i++) {
+        var left = 0;
+        for (var j = 0; j < arr.length; j++) {
+            if (arr[left] > arr[j]) {
+                _a = [arr[j], arr[left]], arr[left] = _a[0], arr[j] = _a[1];
+                left++;
+            }
+        }
+    }
+    return arr;
+}
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        var _this = this;
+        this.collection = collection;
+        this.sort = function () {
+            var length = _this.collection.length;
+            for (var i = 0; i < length; i++) {
+                for (var j = 0; j < length - i - 1; j++) {
+                    if (_this.collection[j] > _this.collection[j + 1]) {
+                        var temp = _this.collection[j];
+                        _this.collection[j] = _this.collection[j + 1];
+                        _this.collection[j + 1] = temp;
+                    }
+                }
+            }
+        };
+    }
+    return Sorter;
+}());
+var sorted = new Sorter([10, 0, -3, 1]);
+// once we called this method we should get the collection is sorted already
+sorted.sort();
+console.log("hello", sorted.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,46 @@
-console.log("hi there")
+
+function bubbleSort(arr:number[]):number[]{
+    // loop over the array 
+    // pick 2 numbers in array and compare them
+    // if the left > right: 
+    //    switch places
+    for(let i = 1 ; i < arr.length ; i++){
+        let left: number = 0
+        for(let j = 0 ; j < arr.length ; j++){
+            if(arr[left] > arr[j]){
+                [arr[left] , arr[j]] = [arr[j] , arr[left]]
+                left ++
+            }
+        }
+    }
+    return arr
+}
 
 
+
+class Sorter{
+    
+    constructor( public collection:number[]) {
+
+    }
+
+    sort = ():void => {
+        const { length } = this.collection
+        for(let i = 0 ; i < length ; i++){
+            for(let j = 0 ; j < length - i - 1 ; j++){
+                if(this.collection[j] > this.collection[j + 1]){
+                    let  temp = this.collection[j] 
+                    this.collection[j] = this.collection[j+1] 
+                    this.collection[j+1] = temp
+                }
+
+            }
+        }
+
+    }
+}
+
+let sorted = new Sorter([10,0,-3,1])
+// once we called this method we should get the collection is sorted already
+sorted.sort()
+console.log("hello",sorted.collection)
